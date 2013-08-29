d11978ed00305325dd220699f584d12335bdf8a2
Chandu Tennety <tennety@valinor.neo>
2013-08-29 17:39:26 -0400

    Spruce up menu

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,6 +1,13 @@
-//= require foundation
+//= require html5
+//= require vendor/custom.modernizr
+//= require jquery
+//= require foundation/foundation
+//= require foundation/foundation.topbar
 //= require jquery.fancybox
 //= require jquery.fancybox-media
 //= require imgsizer
 //= require respond
 
+$(function() {
+  $(document).foundation();
+});
